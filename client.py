d4532157aeb5d639b961f044f71d4c44eaac2756
nyantarou2001002 <156566971+nyantarou2001002@users.noreply.github.com>
2024-03-14 02:09:38 +0900

    Add files via upload

---
+++ client.py
@@ -0,0 +1,37 @@
+import socket
+import sys
+
+sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+
+server_address = '/socket_file'
+print('connecting to {}'.format(server_address))
+
+try:
+    sock.connect(server_address)
+except socket.error as err:
+    print(err)
+    sys.exit(1)
+
+input_message = input('please write your message: ')
+
+try:
+    message = b'Sending a message to the server side'
+    sock.sendall(message)
+
+    sock.settimeout(2)
+
+    try:
+        while True:
+            data = str(sock.recv(32))
+
+            if data:
+                print(data.decode('utf-8'))
+            else:
+                break
+    
+    except(TimeoutError):
+        print('Socket timeout, ending listening for server messages')
+
+finally:
+    print('closing socket')
+    sock.close()
